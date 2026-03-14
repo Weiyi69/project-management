f5093763c22db0d775cfaeade8c5cc375f9930a0
vincent <vjsebastiaaan@gmail.com>
2026-03-14 13:19:39 +0800

    fix server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import "dotenv/config";
-import cors from "cors";
-import { clerkMiddleware } from "@clerk/express";
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 
@@ -11,8 +11,16 @@ app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
 
-app.get("/", (req, res) => res.send("Server is live!"));
+app.get('/', (req, res) => res.send('Server is live!'));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 export default app;
